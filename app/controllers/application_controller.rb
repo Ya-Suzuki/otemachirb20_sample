fd7aeaa5009fc9beb3be47442c60a1d6a4cd4afb
Y.Suzuki <ys3128@gmail.com>
2019-08-14 13:56:00 +0900

    remember_input_data削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  def remember_input_data(object:, params_keys:)
-    object.attributes.slice(params_keys)
-  end
-
   def create_flash_message(object)
     object.errors.keys.map { |key| [key.to_s, object.errors.full_messages_for(key)] }.to_h
   end
